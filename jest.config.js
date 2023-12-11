9e8114033000c65fdf04fd798958562f375637b4
JosephByronLane <2419141@modelo.edu.mx>
2023-12-11 16:34:47 -0600

    added sonar cloud coverage file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+      '**/*.{js,jsx,ts,tsx}', // Adjust the pattern to match your source files
+      '!**/node_modules/**',
+      '!**/vendor/**'
+    ],
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    coverageDirectory: '<rootDir>/coverage',
   };
