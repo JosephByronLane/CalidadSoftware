14fee9b20e3b5c775ce345b4e3ef749612a63138
Osvaldo Martinez <oso_imartinez@hotmail.com>
2023-12-10 23:40:49 -0600

    Pruebas unitarias 2 buenas 1 mala

--- src/view/notas.js
+++ src/view/notas.js
@@ -2,43 +2,94 @@ const textarea = document.getElementById("floatingTextarea");
 const btnguardar = document.querySelector(".guardar");
 let clicked = false;
 
-function addLineBreakOnClick() {
-    textarea.addEventListener("click", function() {
-        if (!clicked) {
-          textarea.value += "\n";
-          clicked = true;
-        }
-    });
-}
+//comenta toda esta parte para hacer las pruebas
+// export function addLineBreakOnClick() {
+//     textarea.addEventListener("click", function() {
+//         if (!clicked) {
+//           textarea.value += "\n";
+//           clicked = true;
+//         }
+//     });
+// }
+
 
 function getViajeID(){
-  btnguardar.addEventListener('click', function() {
-    // Realizar una solicitud para obtener el ID del viaje desde tu API
-    fetch('https://cors-anywhere.herokuapp.com/http://54.196.52.240:3000/viajes/1', {
+   // Realizar una solicitud para obtener el ID del viaje desde tu API
+    fetch('http://54.196.52.240:3000/viaje/1', {
       "method": "GET"
     })
         .then(response => {
             if (response.ok) {
                 return response.json(); // Convertir la respuesta a JSON
             } else {
-              console.log(response)
               throw new Error('Error al obtener el ID del viaje');
             }
 
         })
         .then(data => {
-            const idViaje = data.id; // Suponiendo que el ID del viaje está en la propiedad 'id' del objeto devuelto
-            console.log('ID del viaje:', idViaje);
+          idViaje = data.id;
+          console.log('ID del viaje:', idViaje);
+          
+          // After getting the trip ID, attach the event listener to the "Guardar" button
+          btnguardar.addEventListener('click', function() {
+              if (idViaje !== null) {
+                const textarea = document.querySelector("#floatingTextarea");
+                const contenidoNota = textarea.value.trim(); // Trim to remove leading/trailing whitespace
+          
+                if (contenidoNota === '') {
+                  // Show message for empty note
+                  alert('Please write a note before saving.');
+                  return; // Stop execution if the note is empty
+                }
 
-            // Aquí puedes realizar más acciones con el ID del viaje, como enviarlo a otro endpoint o mostrarlo en la interfaz
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-});
+                PostNotaViaje(idViaje);
+              } else {
+                  console.error('ID del viaje no disponible');
+              }
+          });
+      })
+      .catch(error => {
+          console.error('Error: ', error);
+      });
+  }
+  
+function PostNotaViaje(idViaje) {
+  const textarea = document.querySelector("#floatingTextarea");
+  const contenidoNota = textarea.value; // Use semicolons to end statements
+    
+  fetch(`http://54.196.52.240:3000/viajes/nota`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: idViaje,
+      notas: contenidoNota
+    })
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error('Response:', response); // Log the response for troubleshooting
+        throw new Error('Error al guardar la nota');
+      }
+    })
+    .then(data => {
+      console.log('Nota guardada:', data);
+      // Additional actions after successfully saving the note
+    })
+    .catch(error => {
+      console.error('Error al guardar la nota:', error);
+    });
 }
+  
 
 
-
-addLineBreakOnClick()
+// esta funcion
+// addLineBreakOnClick()
 getViajeID()
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = { getViajeID, PostNotaViaje };
+}
---
+++ src/view/notas.test.js
@@ -0,0 +1,65 @@
+  const { TextEncoder, TextDecoder } = require('util'); // For Node.js versions that support TextEncoder/TextDecoder
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+
+  const { JSDOM } = require('jsdom');
+
+  // Mock the DOM environment before your tests run
+  const { window } = new JSDOM('<!doctype html><html><body></body></html>');
+  global.document = window.document;
+  global.window = window;
+
+
+  const fetchMock = require('jest-fetch-mock');
+  fetchMock.enableMocks(); // Enable mocking fetch globally in your tests
+  const { getViajeID, PostNotaViaje } = require('./notas'); // Replace with your actual file path
+
+  describe('getViajeID function', () => {
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+
+    test('should fetch and get trip ID', async () => {
+      fetch.mockResponseOnce(JSON.stringify({ id: 1 }));
+      
+      await getViajeID();
+
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenCalledWith('http://54.196.52.240:3000/viaje/1', { method: 'GET' });
+    });
+  });
+
+  describe('PostNotaViaje function', () => {
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+
+    test('should post trip note', async () => {
+      fetch.mockResponseOnce(JSON.stringify({}));
+
+      document.body.innerHTML = '<textarea id="floatingTextarea"></textarea>';
+      document.querySelector('#floatingTextarea').value = 'Test note';
+
+      await PostNotaViaje(1);
+
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenCalledWith('http://54.196.52.240:3000/viajes/nota', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: 1, notas: 'Test note' })
+      });
+    });
+
+    test('should not post trip note if textarea is empty', async () => {
+      fetch.mockResponseOnce(JSON.stringify({}));
+      
+      document.body.innerHTML = '<textarea id="floatingTextarea"></textarea>';
+      document.querySelector('#floatingTextarea').value = ''; // Set an empty value for the textarea
+    
+      await PostNotaViaje(1);
+    
+      expect(fetch).not.toHaveBeenCalled(); // Expect fetch NOT to have been called
+    });
+    
+  });
+
